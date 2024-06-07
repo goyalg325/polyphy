45e6980bfb4e240327f9846f50f489efd114a49f
goyalg325 <shaffigoyal774@gmail.com>
2024-06-07 20:53:36 +0530

    added updates section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Hero from "@/components/Hero";
+import News from "@/components/News";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
+   <div className="Home">
    <Hero/>
+   <News/>
+   </div>
    </>
   );
 }
