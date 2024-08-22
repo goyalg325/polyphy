8a2dc961d385247d6bb89a88df6d2e7de2df79dc
goyalg325 <shaffigoyal774@gmail.com>
2024-08-22 19:48:56 +0530

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 'use client';
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import { useMediaQuery } from "@mui/material";
 import dynamic from 'next/dynamic';
 import MenuIcon from "@mui/icons-material/Menu";
@@ -7,19 +8,56 @@ import IconButton from "@mui/material/IconButton";
 import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import CloseIcon from "@mui/icons-material/Close";
+import axios from 'axios';
 
 const Navlinks = dynamic(() => import('./Navlinks'), {
   loading: () => <p>Loading...</p>,
 });
 
-const Navbar = ({ categories, pagesByCategory }) => {
+const Navbar = () => {
   const mobile = useMediaQuery("(max-width:768px)");
   const [open, setOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [pagesByCategory, setPagesByCategory] = useState({});
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("/api/categories");
+        console.log("Fetched categories:", response.data);
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  useEffect(() => {
+    const fetchPagesByCategory = async () => {
+      try {
+        const response = await axios.get("/api/page-by-category");
+        console.log("Fetched pages by category:", response.data.data);
+        setPagesByCategory(response.data.data);
+      } catch (error) {
+        console.error("Error fetching pages by category:", error);
+      }
+    };
+
+    fetchPagesByCategory();
+  }, []);
 
   return (
     <div className="navbar bg-black">
       {!mobile ? (
-        <Navlinks dir="row" sp={4} categories={categories} pagesByCategory={pagesByCategory} />
+        <Navlinks
+          dir="row"
+          sp={4}
+          categories={categories}
+          pagesByCategory={pagesByCategory}
+          setOpen={setOpen}
+        />
       ) : !open ? (
         <IconButton onClick={() => setOpen(true)}>
           <MenuIcon />
@@ -33,15 +71,21 @@ const Navbar = ({ categories, pagesByCategory }) => {
         <Box
           sx={{
             padding: "0.5rem 3rem",
-            width: "100%", 
-            height: "100vh", 
+            width: "100%", // Adjusted drawer width for mobile view
+            height: "100vh", // Set height to full viewport height
             backgroundColor: "black",
             marginTop: mobile ? "1rem" : "0",
-            overflowY: "auto",
+            overflowY: "auto", // Make the drawer scrollable when content exceeds height
           }}
         >
           <div className="navbar">
-            <Navlinks dir="column" sp={4} setOpen={setOpen} categories={categories} pagesByCategory={pagesByCategory} />
+            <Navlinks
+              dir="column"
+              sp={4}
+              categories={categories}
+              pagesByCategory={pagesByCategory}
+              setOpen={setOpen}
+            />
           </div>
         </Box>
       </Drawer>
--- components/Navlinks.jsx
+++ components/Navlinks.jsx
@@ -1,25 +1,61 @@
 import React from "react";
-import { Stack } from "@mui/material";
+import { Stack, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
-const Navlinks = ({ dir, sp, setOpen, categories, pagesByCategory }) => {
+const Navlinks = ({ dir, sp, categories, pagesByCategory, setOpen }) => {
+  const mobile = useMediaQuery("(max-width:768px)");
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const [categorizedPages, setCategorizedPages] = React.useState({});
+
+  React.useEffect(() => {
+    const filterPages = categories.reduce((acc, category) => {
+      acc[category] = pagesByCategory[category] || [];
+      return acc;
+    }, {});
+
+    setCategorizedPages(filterPages);
+  }, [categories, pagesByCategory]);
+
+  const handleClickOpen = () => {
+    setOpenDialog(true);
+  };
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+
   return (
-    <Stack direction={dir} spacing={sp} alignItems="center">
+    <Stack
+      direction={dir}
+      spacing={sp}
+      alignItems={!mobile ? "center" : "left"}
+    >
       <li>
-        <a className="navLinksMain" href={`/`}>Home</a>
+        <a className="navLinksMain" href={`/`}>
+          Home
+        </a>
       </li>
       <li>
-        <a className="navLinksMain" href={`/story`}>Story</a>
+        <a className="navLinksMain" href={`/story`}>
+          Story
+        </a>
       </li>
       <li>
-        <a className="navLinksMain" href={"https://polyphy.readthedocs.io/"} target="_blank" rel="noreferrer">Documentation</a>
+        <a
+          className="navLinksMain"
+          href={"https://polyphy.readthedocs.io/"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Documentation
+        </a>
       </li>
 
-      {/* Render categories and pages */}
       {categories.map((category) => (
         <li className="dropdown-wrapper" key={category}>
           <p className="navLinksMain">{category}</p>
           <ul className="dropdown">
-            {pagesByCategory[category]?.map((page) => (
+            {categorizedPages[category]?.map((page) => (
               <li key={page.title} className="w-full">
                 <a
                   className="block text-ellipsis whitespace-nowrap overflow-hidden max-w-[150px]"
@@ -38,20 +74,59 @@ const Navlinks = ({ dir, sp, setOpen, categories, pagesByCategory }) => {
         <p className="navLinksMain">Community</p>
         <ul className="dropdown">
           <li>
-            <a className="dropdown-links" href="https://github.com/PolyPhyHub">GitHub</a>
+            <a className="dropdown-links" href="https://github.com/PolyPhyHub">
+              GitHub
+            </a>
           </li>
           <li>
-            <a className="dropdown-links" href={`/opportunities`}>Opportunities</a>
+            <a className="dropdown-links" href={`/opportunities`}>
+              Opportunities
+            </a>
+          </li>
+          <li onClick={handleClickOpen}>
+            <a className="dropdown-links">Slack</a>
           </li>
           <li>
-            <a className="dropdown-links" href={`/team`}>Team</a>
+            <a
+              className="dropdown-links"
+              href="https://github.com/PolyPhyHub/PolyPhy/issues"
+            >
+              Discussions
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-links" href={`/team`}>
+              Team
+            </a>
           </li>
         </ul>
       </li>
 
       <li>
-        <a className="navLinksMain" href={`/admin`}>Admin</a>
+        <a className="navLinksMain" href={`/admin`}>
+          Admin
+        </a>
       </li>
+
+      <Dialog open={openDialog} onClose={handleClose}>
+        <DialogTitle>Want to join PolyPhy's Slack Channel?</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            If you want to interact with the developers of PolyPhy and know more
+            about the software, feel free to request{" "}
+            <a style={{ color: "#f7981b" }} href="mailto:oelek@ucsc.edu">
+              Dr. Oskar Elek
+            </a>{" "}
+            for the channel invitation.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button variant="contained" href="mailto:oelek@ucsc.edu">
+            Send Email
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Stack>
   );
 };
